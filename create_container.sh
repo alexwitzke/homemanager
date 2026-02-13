75c87a9f86cecb804071bc843adce177e70abbf9
Alexander Witzke <alex.witzke@googlemail.com>
2026-02-13 12:36:43 +0100

    test

--- create_container.sh
+++ create_container.sh
@@ -2,21 +2,37 @@
 
 container="homemanager"
 
-echo Stopping container
-docker stop $container
-docker rm $container
+echo "Stopping and removing old container..."
+docker stop "$container" 2>/dev/null || true
+docker rm "$container" 2>/dev/null || true
 
-echo Cloning repository 
+echo "Cloning repository..."
 git clone https://github.com/alexwitzke/homemanager.git
 
-cd homemanager/ 
+cd homemanager || { echo "Failed to cd into homemanager"; exit 1; }
 
-echo Build container
-docker build -t $container:latest .
+echo "Building container..."
+docker build -t "$container:latest" .
 
-echo Run container
-docker run -d --name $container -v /mnt/user/appdata/homemanager:/app/config -p 3000:3000 $container:latest
+echo "Starting container..."
+docker run -d \
+    --name "$container" \
+    -v /mnt/user/appdata/homemanager:/app/config \
+    -p 3000:3000 \
+    --log-driver json-file \
+    --log-opt max-size=5m \
+    --log-opt max-file=3 \
+    "$container:latest"
 
-echo Cleaning up
-cd .. 
-rm -rf $container
+if [ $? -eq 0 ]; then
+    echo "Container '$container' successfully started."
+else
+    echo "Failed to start container!"
+    exit 1
+fi
+
+echo "Cleaning up clone directory..."
+cd ..
+rm -rf homemanager
+
+echo "Done."
